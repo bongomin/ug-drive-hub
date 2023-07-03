0ed61a3cb4345418108d41366334f3c5c679ab9b
Bongomin Daniel <bongomindaniel@bposeats.com>
2023-07-03 08:41:59 +0300

    import static image for car card display and clean code

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,29 +1,40 @@
-'use client';
-import React from 'react'
-import { useState } from 'react';
+import React from 'react';
 import Image from 'next/image';
 import { carProps } from '@/types';
+import { calculateCarRent } from '@/utillities'
 
-interface carCardProps {
-    car: carProps
+interface CarCardProps {
+    car: carProps;
 }
 
-function CarCard({ car } : carCardProps) {
-    const { city_mpg, year, make, model, transmission, drive } = car
+function CarCard({ car }: CarCardProps) {
+    const { city_mpg, year, make, model, transmission, drive } = car;
+
+    const carRent = calculateCarRent(city_mpg, year);
+
     return (
-        <div className='car_card group'>
+        <div className="car-card group">
             <div className="car-card__content">
-                <h2 className='car-card__content-title'>
+                <h2 className="car-card__content-title">
                     {make} {model}
-                    </h2>
+                </h2>
             </div>
-            <p>
-                <span>
-                    Car Rent....
-                </span>
+            <p className="flex mt-6 text-[32px] font-extrabold">
+                <span className="self-start text-[14px] font-semibold">$</span>
+                {carRent}
+                <span className="self-end text-[14px] font-medium">/day</span>
             </p>
+            <div className="relative w-full h-40 my-3 object-contain">
+                <Image
+                    src="/hero.png"
+                    alt="car model"
+                    fill
+                    priority
+                    className="object-contain"
+                />
+            </div>
         </div>
-    )
+    );
 }
 
-export default CarCard
+export default CarCard;
