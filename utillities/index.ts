44cab2b117ed1b672b0d36352983baa4f840b8aa
Bongomin Daniel <bongomindaniel@bposeats.com>
2023-07-03 06:59:43 +0300

    add utillity file that fetches data from rapid api

---
+++ utillities/index.ts
@@ -0,0 +1,11 @@
+// fetch all cars from rapid api
+export async function  fetchAllCars() {
+    const headers = {
+        'X-RapidAPI-Key': 'a5f37a32aemsh8e29c0757128c37p116d2ajsn94fe6f31a139',
+        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {headers})
+
+    const results = await response.json();
+}
